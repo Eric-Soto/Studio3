39e78fb969819118d0c22b79d1b68175f9fa3752
EricSoto <ericsoto926@gmail.com>
2020-06-26 20:09:49 -0400

    Studio 3 has been finished

---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,64 @@
+package restaurant;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class MenuItem {
+
+    public MenuItem(String name, double price, String description, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.dateAdded = new Date();
+    }
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private double price;
+    private String description;
+    //appetizer, main course, or desert
+    private String category;
+    private boolean isNew = true;
+    private Date dateAdded;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) throws Exception {
+
+        String[] allowedCategories = {"appetizer","main course", "dessert"};
+        if (Arrays.stream(allowedCategories).anyMatch(category::equals)) {
+            this.category = category;
+        }
+        else {
+            System.out.println("not a category");
+        }
+            //throw new Exception("Invalid category entered. No such category in the menu");
+    }
+
+}
